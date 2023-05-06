4b81220935388f80a5240214f6f7388007ad4bb9
Micaela Kloster <108692944+MicaelaKloster@users.noreply.github.com>
2023-05-06 16:38:31 -0300

    Final Commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,7 @@ const sizes = document.querySelectorAll('.size');
 const colors_circle = document.querySelectorAll('.color_circle');
 const vanss = document.querySelectorAll('.vans');
 const colors = document.querySelectorAll('.color');
-const vansBg = document.querySelectorAll('.color_background');
+const vansBg = document.querySelectorAll('.vans_background');
 
 let prevColor = "#080000";
 let animationEnd = true;
@@ -15,7 +15,7 @@ function changeSize(){
 function changeColor(){
     if(!animationEnd) return;
     let primary = this.getAttribute('primary');
-    let color_circle = this.getAttribute('color_circle');
+    let color_circle = this.getAttribute('color');
     let vans = document.querySelector(`.vans[color="${color_circle}"]`);
     let color = document.querySelector(`.color[color="${color_circle}"]`);
     let prevColorBackground = document.querySelector(`.color[color="${prevColor}"]`);
@@ -34,7 +34,7 @@ function changeColor(){
     color.classList.add('first');
     prevColorBackground.classList.add('second');
 
-    prevColor = color;
+    prevColor = color_circle;
     animationEnd = false;
 
     color.addEventListener('animationend', () => {
@@ -50,10 +50,11 @@ let x = window.matchMedia("(max-width: 1000px)");
 function changeHeight(){
     if(x.matches){
         let vansHeight = vanss[0].offsetHeight;
-        vansBg.style.height = `${vansHeight * 0.9}px`;
+        document.querySelector('.vans_background').style.height = `${vansHeight * 0.9}px`;
     }
     else{
-        vansBg.style.height = "475px";
+        document.querySelector('.vans_background').style.height = "475px";
+
     }
 }
 
